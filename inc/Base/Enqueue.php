7049a3c34105e60604ee3f9a0b522eec9ec49d91
phantrunghieu91 <phantrunghieu1291@gmail.com>
2024-07-04 21:31:49 +0700

    Make sure that js and css only enqueue when login feature is activate

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -4,24 +4,33 @@
  * Enqueue Scripts and Styles
  */
 namespace Inc\Base;
+
 use Inc\Base\BaseController;
-class Enqueue extends BaseController {
-  public function register(){
-    if ( is_admin() ) {
+
+class Enqueue extends BaseController
+{
+  public function register()
+  {
+    if (is_admin()) {
       add_action('admin_enqueue_scripts', array($this, 'enqueueAdminsScripts'));
     } else {
       add_action('wp_enqueue_scripts', array($this, 'enqueueFrontEndScripts'));
     }
   }
 
-  public function enqueueAdminsScripts(){
+  public function enqueueAdminsScripts()
+  {
     wp_enqueue_media();
     wp_enqueue_style('jins-plugin-style', $this->plugin_url . 'assets/css/jins-plugin.min.css', [], null, 'all');
     wp_enqueue_script('jins-plugins-script', $this->plugin_url . 'assets/js/jins-plugin.min.js', ['jquery'], null, true);
   }
-  
-  public function enqueueFrontEndScripts() {
-    wp_enqueue_style('jins-plugin-auth', $this->plugin_url . 'assets/css/jins-plugin-auth.min.css', [], null, 'all');
-    wp_enqueue_script('jins-plugins-auth', $this->plugin_url . 'assets/js/jins-plugin-auth.min.js', ['jquery'], null, true);
+
+  public function enqueueFrontEndScripts()
+  {
+    // check if login_manager is activated
+    if ($this->isActivated('login_manager')) {
+      wp_enqueue_style('jins-plugin-auth', $this->plugin_url . 'assets/css/jins-plugin-auth.min.css', [], null, 'all');
+      wp_enqueue_script('jins-plugins-auth', $this->plugin_url . 'assets/js/jins-plugin-auth.min.js', ['jquery'], null, true);
+    }
   }
 }
